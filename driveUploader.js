b8770c491c4f223614216184da2874637fe67941
Guygoren89 <guygoren89@gmail.com>
2025-04-04 17:41:06 +0300

    Update driveUploader.js

--- driveUploader.js
+++ driveUploader.js
@@ -1,42 +1,30 @@
-const { google } = require('googleapis');
 const fs = require('fs');
-const path = require('path');
-const mime = require('mime-types');
+const { google } = require('googleapis');
 
-const auth = new google.auth.GoogleAuth({
-  scopes: ['https://www.googleapis.com/auth/drive.file']
-});
+async function uploadToDrive(filePath, fileName, folderId) {
+  const auth = new google.auth.GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/drive.file'],
+  });
 
-async function uploadToDrive(filePath, folderId) {
-  const authClient = await auth.getClient();
-  const drive = google.drive({ version: 'v3', auth: authClient });
+  const drive = google.drive({ version: 'v3', auth });
 
-  const fileName = path.basename(filePath);
-  const mimeType = mime.lookup(filePath) || 'application/octet-stream';
+  const fileMetadata = {
+    name: fileName,
+    parents: [folderId],
+  };
+
+  const media = {
+    mimeType: 'video/mp4',
+    body: fs.createReadStream(filePath),
+  };
 
   const response = await drive.files.create({
-    requestBody: {
-      name: fileName,
-      parents: [folderId]
-    },
-    media: {
-      mimeType,
-      body: fs.createReadStream(filePath)
-    }
+    resource: fileMetadata,
+    media: media,
+    fields: 'id, webViewLink, webContentLink',
   });
 
-  const fileId = response.data.id;
-
-  await drive.permissions.create({
-    fileId,
-    requestBody: {
-      role: 'reader',
-      type: 'anyone'
-    }
-  });
-
-  const publicUrl = `https://drive.google.com/file/d/${fileId}/view`;
-  return publicUrl;
+  return response.data;
 }
 
 module.exports = uploadToDrive;
