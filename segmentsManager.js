372fd37f06bcb847781689e9282d83cdaa3e2d5d
Guygoren89 <guygoren89@gmail.com>
2025-05-05 13:37:27 +0300

    Update segmentsManager.js

--- segmentsManager.js
+++ segmentsManager.js
@@ -2,23 +2,49 @@
 
 const segments = {};
 
+// הוספת מקטע חדש למשחק
 function addSegment(matchId, segmentInfo) {
   if (!segments[matchId]) {
     segments[matchId] = [];
   }
-  segments[matchId].push(segmentInfo);
+
+  // אם כבר קיים segmentNumber כזה - מחליפים
+  const existingIndex = segments[matchId].findIndex(
+    s => s.segmentNumber === segmentInfo.segmentNumber
+  );
+  if (existingIndex !== -1) {
+    segments[matchId][existingIndex] = segmentInfo;
+  } else {
+    segments[matchId].push(segmentInfo);
+  }
 }
 
+// החזרת רשימת מקטעים
 function getSegments(matchId) {
   return segments[matchId] || [];
 }
 
+// מחיקת מקטעים שלמים למשחק
 function clearSegments(matchId) {
   delete segments[matchId];
+}
+
+// חיפוש מקטע לפי זמן מוחלט
+function findSegmentByTime(matchId, absoluteTime) {
+  const all = segments[matchId] || [];
+  return all.find(s => absoluteTime >= s.startTime && absoluteTime < s.endTime);
+}
+
+// חיפוש מקטע לפי מספר מקטע
+function getSegmentByNumber(matchId, segmentNumber) {
+  const all = segments[matchId] || [];
+  return all.find(s => s.segmentNumber === segmentNumber);
 }
 
 module.exports = {
   addSegment,
   getSegments,
   clearSegments,
+  findSegmentByTime,
+  getSegmentByNumber,
 };
