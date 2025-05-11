61091ca8ccb55a5a6a09d756d6af33e88b2640e9
Guygoren89 <guygoren89@gmail.com>
2025-05-11 17:57:00 +0300

    Update autoGenerateClips.js

--- autoGenerateClips.js
+++ autoGenerateClips.js
@@ -1,16 +1,16 @@
 const { cutClip } = require('./segmentsManager');
 
-async function autoGenerateClips(fileId, clipTimestamps) {
+async function autoGenerateClips(fileId, clipTimestamps, matchId = "auto_match") {
   const results = [];
 
-  for (const { start_time_in_segment } of clipTimestamps) {
+  for (const { start_time_in_segment, duration = 8, action_type = "auto_clip" } of clipTimestamps) {
     try {
       const result = await cutClip(
         fileId,
         start_time_in_segment,
-        8,               // ברירת מחדל לאורך קליפ
-        "auto_match",    // match_id גנרי
-        "auto_clip"      // action_type גנרי
+        duration,
+        matchId,
+        action_type
       );
       results.push(result);
     } catch (err) {
