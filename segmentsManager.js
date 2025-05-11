e60d6a383d8b671eeb1e6bc743d967202d6de185
Guygoren89 <guygoren89@gmail.com>
2025-05-11 16:07:15 +0300

    Update segmentsManager.js

--- segmentsManager.js
+++ segmentsManager.js
@@ -1,50 +1,80 @@
-// segmentsManager.js
+const { google } = require('googleapis');
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+const { v4: uuidv4 } = require('uuid');
 
-const segments = {};
+const auth = new google.auth.GoogleAuth({
+  scopes: ['https://www.googleapis.com/auth/drive'],
+});
 
-// הוספת מקטע חדש למשחק
-function addSegment(matchId, segmentInfo) {
-  if (!segments[matchId]) {
-    segments[matchId] = [];
-  }
+async function cutClip(fileId, startTimeInSec, durationInSec, matchId, actionType) {
+  const authClient = await auth.getClient();
+  const drive = google.drive({ version: 'v3', auth: authClient });
 
-  // אם כבר קיים segmentNumber כזה - מחליפים
-  const existingIndex = segments[matchId].findIndex(
-    s => s.segmentNumber === segmentInfo.segmentNumber
+  const tempInput = path.join('/tmp', `${uuidv4()}.webm`);
+  const tempOutput = path.join('/tmp', `${uuidv4()}.webm`);
+
+  const dest = fs.createWriteStream(tempInput);
+  const res = await drive.files.get(
+    { fileId, alt: 'media' },
+    { responseType: 'stream' }
   );
-  if (existingIndex !== -1) {
-    segments[matchId][existingIndex] = segmentInfo;
-  } else {
-    segments[matchId].push(segmentInfo);
-  }
+  await new Promise((resolve, reject) => {
+    res.data
+      .on('end', resolve)
+      .on('error', reject)
+      .pipe(dest);
+  });
+
+  const ffmpegCmd = `ffmpeg -ss ${startTimeInSec} -i ${tempInput} -t ${durationInSec} -c copy ${tempOutput}`;
+  await new Promise((resolve, reject) => {
+    exec(ffmpegCmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error('FFmpeg error:', stderr);
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+
+  const uploadRes = await drive.files.create({
+    resource: {
+      name: `clip_${Date.now()}.webm`,
+      parents: ['1onJ7niZb1PE1UBvDu2yBuiW1ZCzADv2c'], // תיקיית Short_clips
+    },
+    media: {
+      mimeType: 'video/webm',
+      body: fs.createReadStream(tempOutput),
+    },
+    fields: 'id',
+  });
+
+  const newClipId = uploadRes.data.id;
+
+  await drive.files.update({
+    fileId: newClipId,
+    resource: {
+      description: JSON.stringify({
+        match_id: matchId,
+        action_type: actionType,
+        source_file_id: fileId,
+        start_time_in_segment: startTimeInSec,
+        duration: durationInSec,
+      }),
+    },
+  });
+
+  fs.unlinkSync(tempInput);
+  fs.unlinkSync(tempOutput);
+
+  return {
+    success: true,
+    file_id: newClipId,
+    source_file_id: fileId,
+    start_time: startTimeInSec,
+    duration: durationInSec,
+  };
 }
 
-// החזרת רשימת מקטעים
-function getSegments(matchId) {
-  return segments[matchId] || [];
-}
-
-// מחיקת מקטעים שלמים למשחק
-function clearSegments(matchId) {
-  delete segments[matchId];
-}
-
-// חיפוש מקטע לפי זמן מוחלט
-function findSegmentByTime(matchId, absoluteTime) {
-  const all = segments[matchId] || [];
-  return all.find(s => absoluteTime >= s.startTime && absoluteTime < s.endTime);
-}
-
-// חיפוש מקטע לפי מספר מקטע
-function getSegmentByNumber(matchId, segmentNumber) {
-  const all = segments[matchId] || [];
-  return all.find(s => s.segmentNumber === segmentNumber);
-}
-
-module.exports = {
-  addSegment,
-  getSegments,
-  clearSegments,
-  findSegmentByTime,
-  getSegmentByNumber,
-};
+module.exports = { cutClip };
