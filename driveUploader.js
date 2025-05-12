f799095431ba7571872ac058fd144a541c2cdee2
Guygoren89 <guygoren89@gmail.com>
2025-05-12 13:12:04 +0300

    Update driveUploader.js

--- driveUploader.js
+++ driveUploader.js
@@ -1,69 +1,38 @@
-// driveUploader.js
-
-const { google } = require('googleapis');
 const fs = require('fs');
+const { google } = require('googleapis');
 const path = require('path');
 
 const auth = new google.auth.GoogleAuth({
   scopes: ['https://www.googleapis.com/auth/drive'],
 });
+const drive = google.drive({ version: 'v3', auth });
 
-async function uploadSegmentToDrive(file, filename, match_id, start_time, end_time) {
-  if (file.mimetype !== 'video/webm') {
-    throw new Error('Invalid file type. Only video/webm is allowed.');
-  }
+const FULL_CLIPS_FOLDER = '1vu6elArxj6YKLZePXjoqp_UFrDiI5ZOC';
 
-  const authClient = await auth.getClient();
-  const drive = google.drive({ version: 'v3', auth: authClient });
-
+async function uploadToDrive(localPath, name, match_id, start_time, end_time) {
   const fileMetadata = {
-    name: filename,
-    parents: ['1vu6elArxj6YKLZePXjoqp_UFrDiI5ZOC'], // Full_clips folder
+    name,
+    parents: [FULL_CLIPS_FOLDER],
   };
-
   const media = {
-    mimeType: file.mimetype,
-    body: fs.createReadStream(file.path),
+    mimeType: 'video/webm',
+    body: fs.createReadStream(localPath),
   };
 
-  const uploadResponse = await drive.files.create({
+  const file = await drive.files.create({
     resource: fileMetadata,
     media,
     fields: 'id',
   });
 
-  const fileId = uploadResponse.data.id;
-
-  await drive.files.update({
-    fileId,
-    resource: {
-      description: JSON.stringify({
-        match_id,
-        start_time,
-        end_time,
-      }),
-    },
-  });
-
-  fs.unlink(file.path, (err) => {
-    if (err) {
-      console.error(`Failed to delete temp file: ${file.path}`, err);
-    } else {
-      console.log(`Temp file deleted: ${file.path}`);
-    }
-  });
-
-  console.log(`[UPLOAD DONE] file_id: ${fileId}, name: ${filename}`);
+  fs.unlink(localPath, () => {}); // מחיקת קובץ זמני
 
   return {
-    success: true,
-    clip: {
-      google_file_id: fileId,
-      name: filename,
-      start_time,
-      end_time,
-    },
+    google_file_id: file.data.id,
+    name,
+    start_time,
+    end_time,
   };
 }
 
-module.exports = { uploadSegmentToDrive };
+module.exports = { uploadToDrive };
