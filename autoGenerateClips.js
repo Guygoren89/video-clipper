8411786614ee0dbcecb0a7c79faae6c7f15db159
Guygoren89 <guygoren89@gmail.com>
2025-05-22 13:50:42 +0300

    Update autoGenerateClips.js

--- autoGenerateClips.js
+++ autoGenerateClips.js
@@ -1,27 +1,32 @@
 const { cutClipFromDriveFile } = require('./segmentsManager');
 
-async function autoGenerateClips(fileId, clipTimestamps, matchId = "auto_match") {
+/**
+ *  clipTimestamps = [
+ *    { start_time_in_segment: 7, duration: 8, action_type: 'goal' },
+ *    ...
+ *  ]
+ */
+async function autoGenerateClips(fileId, clipTimestamps, matchId = `auto_match_${Date.now()}`) {
   const results = [];
 
-  for (const { start_time_in_segment, duration = 8, action_type = "auto_clip" } of clipTimestamps) {
+  for (const { start_time_in_segment, duration = 8, action_type = 'auto_clip' } of clipTimestamps) {
     try {
       const result = await cutClipFromDriveFile({
         fileId,
         startTimeInSec: start_time_in_segment,
-        durationInSec: duration,
+        durationInSec : duration,
         matchId,
-        actionType: action_type
+        actionType    : action_type
       });
       results.push(result);
     } catch (err) {
       results.push({
-        success: false,
-        error: err.message,
+        success : false,
+        error   : err.message,
         start_time_in_segment
       });
     }
   }
-
   return results;
 }
 
